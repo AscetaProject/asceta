35d773f4e92bb6d9515614d59b8cea04988ce3f3
mjimenez <mjimenez@40aa32a4-4989-4449-82af-b8213426f144>
2011-06-10 09:36:05 +0000

    Creado el modulo de mediawiki para moodle

---
+++ modmediawiki/lang/en/modmediawiki.php
@@ -0,0 +1,39 @@
+<?php
+
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English strings for modmediawiki
+ *
+ * You can have a rather longer description of the file as well,
+ * if you like, and it can span multiple lines.
+ *
+ * @package   mod_modmediawiki
+ * @copyright 2010 Your Name
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die();
+
+$string['modulename'] = 'modmediawiki';
+$string['modulenameplural'] = 'modmediawikis';
+$string['modmediawikifieldset'] = 'Custom example fieldset';
+$string['modmediawikiname'] = 'modmediawiki name';
+$string['modmediawikiname_help'] = 'This is the content of the help tooltip associated with the modmediawikiname field. Markdown syntax is supported.';
+$string['modmediawiki'] = 'modmediawiki';
+$string['pluginadministration'] = 'modmediawiki administration';
+$string['pluginname'] = 'modmediawiki';
