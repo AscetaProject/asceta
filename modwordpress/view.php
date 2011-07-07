e36ebdc61bc083ceac23d7994338807ec3e27353
vmgarcia <vmgarcia@40aa32a4-4989-4449-82af-b8213426f144>
2011-07-07 11:24:31 +0000

    - Visualización detallada de un Post (con comentarios) - Integración de Páginas de Wordpress (post con post_type 'page')

--- modwordpress/view.php
+++ modwordpress/view.php
@@ -39,6 +39,8 @@ $comments = optional_param('comments', 0, PARAM_INT); // Post ID to get comments
 $new_comment = optional_param('new_comment', 0, PARAM_INT); // Post ID to get comments
 $comment_content = optional_param('comment_content', '', PARAM_TEXT); // Post ID to get comments
 $comment_post_ID = optional_param('comment_post_ID', 0, PARAM_INT); // Post ID to get comments
+$post = optional_param('post', 0, PARAM_INT);
+$page = optional_param('page', 0, PARAM_INT);
 $new_post = optional_param('new_post', '', PARAM_TEXT); // Post ID to get comments
 $post_title = optional_param('post_title', '', PARAM_TEXT); // Post ID to get comments
 $post_content = optional_param('post_content', '', PARAM_TEXT); // Post ID to get comments
@@ -141,22 +143,65 @@ if (!$modwordpress->server_id) {
 	    echo "</div>";
 	    echo "<div class='clearfix' style='margin: 5px 10px;'>$comment->comment_content</div>";
 	    echo "<div class='clearfix' style='margin: 5px 10px; color: gray; font-size: 90%;'>";
-	    echo "<a href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;new_comment=$json->ID&amp;sesskey=" . sesskey() . "'>Deja un comentario</a>";
+	    echo "<a href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;new_comment=$json->ID&amp;sesskey=" . sesskey() . "'>".  get_string("comment_post", "modwordpress")."</a>";
 	    echo "</div>";
 	    echo "</div>";
 	}
         }
         echo "<button onclick='javascript:history.back()'>Volver</button>  ";
+    } elseif ($post and confirm_sesskey()) {
+        $consumer = new OAuthConsumer($server->consumer_key, $server->consumer_secret, NULL);
+        $token = new OAuthToken($server->access_token, $server->access_secret, NULL);
+        $basefeed = rtrim($server->url, '/') . "/post/$post.json";
+        $request = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $basefeed, array());
+        $request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, $token);
+        $response = send_request($request->get_normalized_http_method(), $basefeed, $request->to_header());
+        $post = json_decode($response);
+        if (count($post)) {
+	echo $OUTPUT->heading($modwordpress->name." : ".$post->post_title);
+	echo "<p style='font-size: 75%; color: gray;'>Publicado en $post->post_date por $post->post_author</p>";
+	echo $post->post_content;
+	echo "<br/><br/><a href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;new_comment=$post->ID&amp;sesskey=" . sesskey() . "'>".  get_string("comment_post", "modwordpress")."</a>";
+	if (isset ($post->comments)) {
+	    foreach ($post->comments as $comment) {
+	        echo "<div id='$comment->comment_ID' style='margin-bottom: 50px;'>";
+	        echo "<div class='navbar clearfix' style='border: 1px solid #DDD; padding: 1px;'>";
+	        echo "<span style='margin: 0; font-weight:bold'>$comment->comment_author</span> dijo:";
+	        echo "<p style='font-size: 75%; color: gray;'>Publicado en $comment->comment_date</p>";
+	        echo "</div>";
+	        echo "<div class='clearfix' style='margin: 5px 10px;'>$comment->comment_content</div>";
+	        echo "<div class='clearfix' style='margin: 5px 10px; color: gray; font-size: 90%;'>";
+	        echo "</div>";
+	        echo "</div>";
+	    }
+	echo "<a href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;new_comment=$post->ID&amp;sesskey=" . sesskey() . "'>".  get_string("comment_post", "modwordpress")."</a>";
+	}
+        }
+        echo "<br/><button style='margin-top: 20px;' onclick='javascript:history.back()'>Volver</button>  ";
+    } elseif ($page and confirm_sesskey()) {
+        $consumer = new OAuthConsumer($server->consumer_key, $server->consumer_secret, NULL);
+        $token = new OAuthToken($server->access_token, $server->access_secret, NULL);
+        $basefeed = rtrim($server->url, '/') . "/page/$page.json";
+        $request = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $basefeed, array());
+        $request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, $token);
+        $response = send_request($request->get_normalized_http_method(), $basefeed, $request->to_header());
+        $post = json_decode($response);
+        if (count($post)) {
+	echo $OUTPUT->heading($modwordpress->name." : ".$post->post_title);
+	echo "<p style='font-size: 75%; color: gray;'>Publicado en $post->post_date por $post->post_author</p>";
+	echo $post->post_content;
+        }
+        echo "<br/><button style='margin-top: 20px;' onclick='javascript:history.back()'>Volver</button>  ";
     } elseif ($new_comment and confirm_sesskey()) {
         echo $OUTPUT->heading($modwordpress->name);
         echo '<form name="new_comment_form" method="post" action="view.php" id="new_comment_form" onsubmit="return new_comment_form_validation();">';
-        echo '<p>Escriba su comentario:</p>';
+        echo "<p>".  get_string("write_comment","modwordpress")."</p>";
         echo '<textarea cols=90 rows=10 name="comment_content"></textarea>';
         echo "<input type='hidden' name='sesskey' value='" . sesskey() . "' />";
         echo "<input type='hidden' name='comment_post_ID' value='$new_comment' />";
         echo "<input type='hidden' name='id' value='$cm->id' />";
-        echo "<br><input type='submit' value='Guardar' />";
-        echo "<button onclick='javascript:history.back()'>Volver</button>  ";
+        echo "<br><input type='submit' value='".get_string("save","modwordpress")."' />";
+        echo "<button onclick='javascript:history.back()'>".get_string("back","modwordpress")."</button>  ";
         echo '</form>';
         echo " <script type='text/javascript'> function new_comment_form_validation() { if (document.new_comment_form.comment_content.value.length == 0) { alert('" . print_string('comment_empty', 'modwordpress') . "'); document.new_comment_form.comment_content.focus(); return false; } }</script>";
     } elseif ( $new_post != '' and confirm_sesskey()) {
@@ -164,13 +209,13 @@ if (!$modwordpress->server_id) {
         echo '<form name="new_post_form" method="post" action="view.php" id="new_post_form" onsubmit="return new_post_form_validation();">';
         echo '<table><thead></thead><tbody>';
         echo '<tr>';
-        echo "<td><label for='post_title'>Título</label></td>";
+        echo "<td><label for='post_title'>".get_string("title","modwordpress")."</label></td>";
         echo "<td><input type='text' name='post_title' value='' size='80px' /></td>";
         echo "</tr><tr>";
         echo "<td colspan='2'><textarea cols=90 rows=10 name='post_content'></textarea></td>";
         echo "</tr></tbody></table>";
-        echo "<input type='submit' value='Guardar' />";
-        echo "<button onclick='javascript:history.back()'>Volver</button>  ";
+        echo "<input type='submit' value='".get_string("save", "modwordpress")."' />";
+        echo "<button onclick='javascript:history.back()'>".get_string("back","modwordpress")."</button>  ";
         echo "<input type='hidden' name='sesskey' value='" . sesskey() . "' />";
         echo "<input type='hidden' name='id' value='$cm->id' />";
         echo '</form>';
@@ -189,10 +234,21 @@ if (!$modwordpress->server_id) {
         echo "}";
         echo "</script>";
     } else {
-        echo $OUTPUT->heading($modwordpress->name);
-        echo "<a style='margin: 5px 10px 20px 10px;' href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;new_post=post&amp;sesskey=" . sesskey() . "'>Nuevo Post</a>";
         $consumer = new OAuthConsumer($server->consumer_key, $server->consumer_secret, NULL);
         $token = new OAuthToken($server->access_token, $server->access_secret, NULL);
+        $basefeed = rtrim($server->url, '/') . '/pages.json';
+        $request = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $basefeed, array());
+        $request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, $token);
+        $response = send_request($request->get_normalized_http_method(), $basefeed, $request->to_header());
+        $json = json_decode($response);
+
+        echo $OUTPUT->heading($modwordpress->name);
+        echo "<a style='margin: 5px 10px 20px 10px;' href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;new_post=post&amp;sesskey=" . sesskey() . "'>Nuevo Post</a>";
+        foreach ($json as $page) {
+	if ($page->post_title != 'api')
+	    echo " | <a style='margin: 5px 10px 20px 10px;' href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;page=$page->ID&amp;sesskey=" . sesskey() . "'>$page->post_title</a>";
+        }
+
         $basefeed = rtrim($server->url, '/') . '/posts.json';
         $request = OAuthRequest::from_consumer_and_token($consumer, $token, 'GET', $basefeed, array());
         $request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $consumer, $token);
@@ -201,7 +257,7 @@ if (!$modwordpress->server_id) {
 
         foreach ($json as $post) {
 	echo "<div id='$post->ID' style='margin-bottom: 50px;'>";
-	echo "<div class='navbar clearfix' style='border: 1px solid #DDD; padding: 5px;'><h3 style='margin: 0;'>$post->post_title</h3>";
+	echo "<div class='navbar clearfix' style='border: 1px solid #DDD; padding: 5px;'><h3 style='margin: 0;'><a style='margin: 5px 10px 20px 10px;' href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;post=$post->ID&amp;sesskey=" . sesskey() . "'>$post->post_title</a></h3>";
 	echo "<p style='font-size: 75%; color: gray;'>Publicado en $post->post_date por $post->post_author</p>";
 	echo "</div>";
 	echo "<div class='clearfix' style='margin: 5px 10px;'>$post->post_content</div>";
@@ -215,7 +271,7 @@ if (!$modwordpress->server_id) {
 	    echo "$post->comment_count Comentario";
 	    echo "</a> | ";
 	}
-	echo "<a href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;new_comment=$post->ID&amp;sesskey=" . sesskey() . "'>Deja un comentario</a>";
+	echo "<a href='$CFG->wwwroot/mod/modwordpress/view.php?id=$cm->id&amp;new_comment=$post->ID&amp;sesskey=" . sesskey() . "'>".  get_string("comment_post", "modwordpress")."</a>";
 	echo "</div>";
 	echo "</div>";
         }
